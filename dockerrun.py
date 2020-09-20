6e2b5d0221a637518a6dbda218ab2edde02ff96c
Naurel1467 <ravitejamerugu@gmail.com>
2020-09-20 16:06:39 +0530

    flexi\bilty to 'input' different OS images by client

--- dockerrun.py
+++ dockerrun.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
+#/var/lib/tomcat9/webapps/ROOT/WEB-INF/cgi  <------locatioon of cgi file in tomcat9
 print("content-type: text/html")
 print()
 import subprocess as sp
 import cgi
 form = cgi.FieldStorage()
-osname = form.getvalue("x")
+osname = form.getvalue("x")    
+osimage = cgi.FieldStorage("i")             # 'x' is the input value from user
 #osname = input("enter ur OS name: ")
-cmd = docker run -d -it --name {} ubuntu:14.04.format(osname)
-output = sp.getstatusoutput(cmd)
+#to call the value of 'x' from above 'osname' variable we need to use {} .format(osname)
+cmd = "sudo docker run -d -it --name {} {} ".format(osname,osimage)    # 'x' is stored in osname and get called by cmd
+output = sp.getstatusoutput(cmd)   #this function(subprocess alias sp) stores its output in variable called "output"
 status = output[0]
-result = output[1]
+result = output[1] 
 if status == 0:
     print("OS launched {}".format(osname))
 else:
